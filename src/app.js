04fa90018025b544a16deedc92b4e183d0fc71e9
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-05-19 18:13:07 +0000

    ahora si que funciona

--- src/app.js
+++ src/app.js
@@ -6,6 +6,64 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const newCard = () => {
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    console.log(randomNumber);
+    let putNumber = document.getElementById("number-card");
+    putNumber.textContent = randomNumber;
+  };
+
+  //Función que cambia el color del palo correspondiente
+
+  const randomSuitCard = () => {
+    const suits = ["♦", "♥", "♠", "♣"];
+    let suit = Math.floor(Math.random() * suits.length);
+    let headerSuit = document.getElementById("header-number");
+    let footerSuit = document.getElementById("footer-number");
+
+    if (suit === 0) {
+      headerSuit.textContent = "♣";
+      footerSuit.textContent = "♣";
+      headerSuit.style.color = "black";
+      footerSuit.style.color = "black";
+    } else if (suit === 1) {
+      headerSuit.textContent = "♠";
+      footerSuit.textContent = "♠";
+      headerSuit.style.color = "black";
+      footerSuit.style.color = "black";
+    } else if (suit === 2) {
+      headerSuit.textContent = "♥";
+      footerSuit.textContent = "♥";
+      headerSuit.style.color = "red";
+      footerSuit.style.color = "red";
+    } else {
+      headerSuit.textContent = "♦";
+      footerSuit.textContent = "♦";
+      headerSuit.style.color = "red";
+      footerSuit.style.color = "red";
+    }
+  };
+
+  document.querySelector("#button-new-card").addEventListener("click", () => {
+    randomSuitCard();
+    newCard();
+  });
+  newCard();
+  randomSuitCard();
 };
